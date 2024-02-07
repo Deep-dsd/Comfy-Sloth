2040cdd39a6821d417fd75a727a1fdd20c078fd1
Deep Sankar De <78144390+Deep-dsd@users.noreply.github.com>
2024-02-07 15:14:46 +0530

    detatch stripe

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -47,15 +47,15 @@ const CheckoutForm = () => {
   };
 
   const createPaymentIntent = async () => {
-    try {
-      const data = await axios.post(
-        "/.netlify/functions/create-payment-intent",
-        JSON.stringify({ cart, shippingFee, totalAmount })
-      );
-      console.log(data);
-    } catch (error) {
-      console.log(error.response);
-    }
+    // try {
+    //   const data = await axios.post(
+    //     "/.netlify/functions/create-payment-intent",
+    //     JSON.stringify({ cart, shippingFee, totalAmount })
+    //   );
+    //   console.log(data);
+    // } catch (error) {
+    //   console.log(error.response);
+    // }
   };
 
   useEffect(() => {
